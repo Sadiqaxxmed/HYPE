bf9414797ade2b21f52e1ce5b47dd5803da22187
sadiqaxxmed <sadiqaxxmed@gmail.com>
2024-08-12 23:14:51 -0700

    project setup

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://offsznahmed:Offszn25!@cluster0.rz5qp.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
